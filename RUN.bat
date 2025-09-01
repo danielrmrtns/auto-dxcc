baa96a1ee22dab238200969f01d6387b4b1b2867
Daniel <danielrmartins00@gmail.com>
2025-09-01 15:46:29 +0100

    added extra lib installation

--- RUN.bat
+++ RUN.bat
@@ -6,6 +6,12 @@ echo PyMuPDF
 python -m pip install --quiet PyMuPDF
 echo openpyxl
 python -m pip install --quiet openpyxl
+echo requests
+python -m pip install --quiet requests
+echo fitz
+python -m pip install --quiet fitz
+echo shutil
+python -m pip install --quiet shutil
 
 echo.
 echo Running the script...
